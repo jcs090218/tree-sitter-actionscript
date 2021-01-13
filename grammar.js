0011a3bc457b2f5ed2c645789a59cadece8797f4
JenChieh <jcs090218@gmail.com>
2021-01-14 02:49:22 +0800

    Add first grammar.

---
+++ grammar.js
@@ -0,0 +1,18 @@
+/**
+ * $File: grammar.js $
+ * $Date: 2021-01-13 20:59:33 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2021 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+module.exports = grammar({
+  name: 'actionscript',
+
+  rules: {
+    source_file: $ => 'hello'
+  }
+});
