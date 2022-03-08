1ccdd1adec7bdd83f2f9b4d4f52fece8788ab500
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-09 03:22:47 +0800

    Update grammar

--- grammar.js
+++ grammar.js
@@ -39,86 +39,152 @@ module.exports = grammar({
   word: $ => $.identifier,
 
   rules: {
-    identifier: $ => /[A-Za-z_$][A-Za-z0-9_$]*/,
-    semicolon: $ => ';',
-    this: $ => 'this',
-    super: $ => 'super',
+    //
+    // Expression components
+    //
 
-    // C Style comment
+    //
+    // Expressions
+    //
+
+    //
+    // Primitives
+    //
+    string: $ => choice(
+      seq(
+        '"',
+        repeat(choice(
+          alias($.unescaped_double_string_fragment, $.string_fragment),
+          $.escape_sequence
+        )),
+        '"'
+      ),
+      seq(
+        "'",
+        repeat(choice(
+          alias($.unescaped_single_string_fragment, $.string_fragment),
+          $.escape_sequence
+        )),
+        "'"
+      )
+    ),
+
+    unescaped_double_string_fragment: $ =>
+    token.immediate(prec(1, /[^"\\]+/)),
+
+    // same here
+    unescaped_single_string_fragment: $ =>
+      token.immediate(prec(1, /[^'\\]+/)),
+
+    escape_sequence: $ => token.immediate(seq(
+      '\\',
+      choice(
+        /[^xu0-7]/,
+        /[0-7]{1,3}/,
+        /x[0-9a-fA-F]{2}/,
+        /u[0-9a-fA-F]{4}/,
+        /u{[0-9a-fA-F]+}/
+      )
+    )),
+
     comment: $ => token(choice(
       seq('//', /.*/),
       seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/'))),
 
-      // Declarations
+    number: $ => {
+      const hex_literal = seq(
+        choice('0x', '0X'),
+          /[\da-fA-F](_?[\da-fA-F])*/
+      )
 
-      declaration: $ => prec(1, choice(
-        //$.module_declaration,
-        $.package_declaration,
-        $.import_declaration,
-        //$.class_declaration,
-        //$.interface_declaration,
-        //$.annotation_type_declaration,
-        //$.enum_declaration,
-      )),
+      const decimal_digits = /\d(_?\d)*/
+      const signed_integer = seq(optional(choice('-', '+')), decimal_digits)
+      const exponent_part = seq(choice('e', 'E'), signed_integer)
 
-      package_declaration: $ => seq(
-        repeat($._annotation),
-        'package',
-        $._name,
-        ';'
-      ),
+      const binary_literal = seq(choice('0b', '0B'), /[0-1](_?[0-1])*/)
 
-      import_declaration: $ => seq(
-        'import',
-        optional('static'),
-        $._name,
-        optional(seq('.', $.asterisk)),
-        ';'
-      ),
+      const octal_literal = seq(choice('0o', '0O'), /[0-7](_?[0-7])*/)
 
-      // Statements
+      const bigint_literal = seq(choice(hex_literal, binary_literal, octal_literal, decimal_digits), 'n')
 
-      empty_statement: $ => ';',
+      const decimal_integer_literal = choice(
+        '0',
+        seq(optional('0'), /[1-9]/, optional(seq(optional('_'), decimal_digits)))
+      )
 
-      _statement: $ => choice(
-        //$.export_statement,
-        //$.import_statement,
-        //$.debugger_statement,
-        //$.expression_statement,
-        //$._declaration,
-        //$.statement_block,
+      const decimal_literal = choice(
+        seq(decimal_integer_literal, '.', optional(decimal_digits), optional(exponent_part)),
+        seq('.', decimal_digits, optional(exponent_part)),
+        seq(decimal_integer_literal, exponent_part),
+        seq(decimal_digits),
+      )
 
-        //$.if_statement,
-        //$.switch_statement,
-        //$.for_statement,
-        //$.for_in_statement,
-        //$.while_statement,
-        //$.do_statement,
-        //$.try_statement,
-        //$.with_statement,
+      return token(choice(
+        hex_literal,
+        decimal_literal,
+        binary_literal,
+        octal_literal,
+        bigint_literal,
+      ))
+    },
 
-        //$.break_statement,
-        //$.continue_statement,
-        //$.return_statement,
-        //$.throw_statement,
-        $.empty_statement,
-        //$.labeled_statement
-      ),
+    identifier: $ => {
+      const alpha = /[^\x00-\x1F\s\p{Zs}0-9:;`"'@#.,|^&<=>+\-*/\\%?!~()\[\]{}\uFEFF\u2060\u200B]|\\u[0-9a-fA-F]{4}|\\u\{[0-9a-fA-F]+\}/
+      const alphanumeric = /[^\x00-\x1F\s\p{Zs}:;`"'@#.,|^&<=>+\-*/\\%?!~()\[\]{}\uFEFF\u2060\u200B]|\\u[0-9a-fA-F]{4}|\\u\{[0-9a-fA-F]+\}/
+                         return token(seq(alpha, repeat(alphanumeric)))
+                        },
 
-      statement_block: $ => prec.right(seq(
-        '{',
-        repeat($._statement),
-        '}',
-        optional($.semicolon)
-      )),
+                    semicolon: $ => ';',
 
-      function_declaration: $ => prec.right(PREC.DECLARATION, seq(
-        optional('async'),
-        'function',
-        field('name', $.identifier),
-        $._call_signature,
-        field('body', $.statement_block),
-        optional($.semicolon)
-      )),
-    }
-                       });
+                    this: $ => 'this',
+                    super: $ => 'super',
+                    true: $ => 'true',
+                    false: $ => 'false',
+                    null: $ => 'null',
+
+                    //
+                    // Expression components
+                    //
+
+                    arguments: $ => seq(
+                      '(',
+                      commaSep(optional(choice($.expression, $.spread_element))),
+                      ')'
+                    ),
+
+                    _property_name: $ => choice(
+                      alias(choice(
+                        $.identifier,
+                        $._reserved_identifier
+                      ), $.property_identifier),
+                      $.private_property_identifier,
+                      $.string,
+                      $.number,
+                      $.computed_property_name
+                    ),
+
+                    computed_property_name: $ => seq(
+                      '[',
+                      $.expression,
+                      ']'
+                    ),
+
+                    _reserved_identifier: $ => choice(
+                      'get',
+                      'set',
+                      'async',
+                      'static',
+                      'export'
+                    ),
+
+                    _semicolon: $ => choice($._automatic_semicolon, ';')
+                   }
+  });
+
+                         function commaSep1(rule) {
+                           return seq(rule, repeat(seq(',', rule)));
+                         }
+
+                         function commaSep(rule) {
+                           return optional(commaSep1(rule));
+                         }
